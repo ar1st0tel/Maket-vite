9e0ea53efcd259f501ecfb42c91f6ca925d1a4bb
Egor <Shambasovegor@yandex.ru>
2024-10-02 17:57:34 +0400

    Commit1

--- src/info/info.tsx
+++ src/info/info.tsx
@@ -1,14 +1,19 @@
 import classes from "./info.module.css";
 import Preview from "./preview/preview.tsx";
 import Stats from "./preview/stats/stats.tsx";
+import Pic1, {Pic2, Pic3, Pic4} from "./preview/pics/pics.tsx";
 
 const Info = () => {
     return (
         <div className={classes.infoBox}>
             <div className={`${classes.item} ${classes.item1}`}><Preview/></div>
             <div className={`${classes.item} ${classes.item2}`}><Stats/></div>
-            <div className={`${classes.item} ${classes.item3}`}>pics</div>
+            <div className={`${classes.item} ${classes.item3}`}><Pic1/>
+                <Pic2/>
+                <Pic3/>
+                <Pic4/>
+            </div>
         </div>
-    )
+    );
 }
 export default Info
--- src/info/preview/pics/pics.tsx
+++ src/info/preview/pics/pics.tsx
@@ -1,10 +1,37 @@
 import classes from '../../info.module.css'
 
-const Pics () => {
+const Pic1 = () => {
     return (
-        <div className={classes.item3}>
-            pics
+            <div className={classes.bigPic}>
+            <img src="http://nordclan.com/uploads/a9200c5147a4421d8c2dcef2d15a1691.png" alt="1"/>
         </div>
     )
 }
-export default Pics;
+export default Pic1;
+
+const Pic2 = () => {
+    return (
+        <>
+            <img className={classes.arrow} src="http://nordclan.com/uploads/47ceae2db706497bb684567415f379a7.png" alt="1"/>
+        </>
+    )
+}
+export {Pic2};
+
+    const Pic3 = () => {
+        return (
+            <div className={classes.dots}>
+                <img src="http://nordclan.com/uploads/ca7483673b3b4daa9c4d9df4af5798b2.png" alt="1"/>
+            </div>
+        )
+    }
+export {Pic3};
+
+const Pic4 = () => {
+    return (
+        <div className={classes.smallPic}>
+            <img src="http://nordclan.com/uploads/f6bca011c78346469d7cf97cca283aee.png" alt="1"/>
+        </div>
+    )
+}
+export {Pic4};
