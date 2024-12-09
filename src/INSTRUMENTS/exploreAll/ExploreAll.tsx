1ffba12790f6c6bc4cd09bc00f422cddeae6844b
Egor <Shambasovegor@yandex.ru>
2024-12-09 11:54:56 +0400

    в файле pageTest тестовые задания: туду лист и каунтер(не для этого проекта)

--- src/INSTRUMENTS/exploreAll/ExploreAll.tsx
+++
@@ -1,8 +0,0 @@
-import classes from "../../content3/exploreMarketplace/exploreMarketplace.module.scss"
-
-const ExploreAll = () =>{
-    return (<div className={classes.explore}>
-        <a className={classes.a} href="#S">Explore All →</a>
-    </div>)
-}
-export default ExploreAll;
--- src/MobileComponents/MobileCardsPreview/CardsPreview.tsx
+++
@@ -1,9 +0,0 @@
-import {HeroPictures} from "../../Hero/hero.tsx";
-
-const CardsPreview = () => {
-    return (<div style={{marginLeft: 30, marginBottom: 110}}>
-            <HeroPictures/>
-        </div>
-    )
-}
-export default CardsPreview;
